bc49fb573e6c7b3f295f81aceb475b76ef08c172
Aashika <srivastava.aashika06@gmail.com>
2026-07-14 03:30:26 +0530

    fix:patch edge weight evaluation logic during dynamic pathfinding

--- src/traffic/model/Edge.java
+++ src/traffic/model/Edge.java
@@ -3,7 +3,7 @@ package traffic.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
+/*import java.util.Objects;*/
 
 /**
  * A directed edge (road segment) connecting two GridNodes.
